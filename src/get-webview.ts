7b0a2c73756826cf4ef091aea5b46599da70a465
TheCodeTherapy <me@mgz.me>
2024-09-07 16:15:45 +0100

    updates path to be used to fetch the webview assets

--- src/get-webview.ts
+++ src/get-webview.ts
@@ -7,17 +7,17 @@ export function getWebviewContent(
   panel: vscode.WebviewPanel
 ): string {
   const htmlPath = vscode.Uri.file(
-    path.join(context.extensionPath, "src", "webview", "index.html")
+    path.join(context.extensionPath, "dist", "webview", "index.html")
   );
   let htmlContent = fs.readFileSync(htmlPath.fsPath, "utf8");
   const styleUri = panel.webview.asWebviewUri(
     vscode.Uri.file(
-      path.join(context.extensionPath, "src", "webview", "style.css")
+      path.join(context.extensionPath, "dist", "webview", "style.css")
     )
   );
   const scriptUri = panel.webview.asWebviewUri(
     vscode.Uri.file(
-      path.join(context.extensionPath, "src", "webview", "script.js") // This should be a bundled file that includes your dependencies
+      path.join(context.extensionPath, "dist", "webview", "script.js") // This should be a bundled file that includes your dependencies
     )
   );
   htmlContent = htmlContent.replace('href="style.css"', `href="${styleUri}"`);
